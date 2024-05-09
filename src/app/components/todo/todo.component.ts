9a984ddcc36fdea42ac044fe3a8905f752c7d6f3
Cristian Fernando <cristianfernandovilluendas@gmail.com>
2024-05-09 20:52:32 +0200

    app terminada

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, computed, effect, signal } from '@angular/core';
 import { FilterType } from '../../models/todo';
 import { CommonModule } from '@angular/common';
 import { TodoModel } from '../../models/todo';
@@ -11,34 +11,43 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
-export class TodoComponent {
-  todoList = signal<TodoModel[]>([
-    {
-      id: 1,
-      title: 'Comprar leche',
-      completed: false,
-      editing: false,
-    },
-    {
-      id: 2,
-      title: 'Comprar pan',
-      completed: false,
-      editing: false,
-    },
-    {
-      id: 3,
-      title: 'Comprar queso',
-      completed: false,
-      editing: false,
-    },
-  ])
+export class TodoComponent implements OnInit {
+  todoList = signal<TodoModel[]>([])
 
   filter = signal<FilterType>('all');
+
+  todoListFiltered = computed(() => {
+    const filter = this.filter();
+    const todos = this.todoList();
+    switch (filter) {
+      case 'active':
+        return todos.filter((todo) => !todo.completed);
+      case 'completed':
+        return todos.filter((todo) => todo.completed);
+      default:
+        return todos;
+    }
+  })
 
   newTodo = new FormControl('', {
     nonNullable: true,
     validators: [Validators.required, Validators.minLength(1)]
   })
+
+  constructor() {
+    effect(() => {
+      localStorage.setItem('todos', JSON.stringify(this.todoList()))
+    })
+  }
+
+  ngOnInit(): void {
+    const storage = localStorage.getItem('todos');
+    if (storage) {
+      this.todoList.set(JSON.parse(storage));
+    }
+  }
+
+
 
   changeFilter(filterString: FilterType) {
     this.filter.set(filterString);
@@ -64,10 +73,33 @@ export class TodoComponent {
     return this.todoList.update((prev_todos) =>
       prev_todos.map((todo) => {
 
-        return todo.id === todoId? {...todo, completed: !todo.completed}
-        :todo;
+        return todo.id === todoId ? { ...todo, completed: !todo.completed }
+          : todo;
+      })
+    )
+  }
+  removeTodo(todoId: number) {
+    this.todoList.update((prev_todods) =>
+      prev_todods.filter((todo) => todo.id !== todoId)
+    )
+  }
+
+  updateTodoEditingMode(todoId: number) {
+    return this.todoList.update((prev_todos) =>
+      prev_todos.map((todo) => {
+        return todo.id === todoId
+          ? { ...todo, editing: true }
+          : { ...todo, editing: false };
       })
     )
   }
 
+  saveTitleTodo(todoId: number, event: Event) {
+    const title = (event.target as HTMLInputElement).value;
+    return this.todoList.update((prev_todos) => prev_todos.map((todo) => {
+      return todo.id === todoId
+        ? { ...todo, title: title, editing: false }
+        : todo;
+    }))
+  }
 }
