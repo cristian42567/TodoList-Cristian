9f393b2eabce8e846fe8e93aee28178eebc127aa
Cristian Fernando <cristianfernandovilluendas@gmail.com>
2024-05-09 19:46:33 +0200

    reactividad

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { FilterType } from '../../models/todo';
 import { CommonModule } from '@angular/common';
 import { TodoModel } from '../../models/todo';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
@@ -21,7 +22,7 @@ export class TodoComponent {
     {
       id: 2,
       title: 'Comprar pan',
-      completed: false,
+      completed: true,
       editing: false,
     },
     {
@@ -34,8 +35,29 @@ export class TodoComponent {
 
   filter = signal<FilterType>('all');
 
+  newTodo = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(1)]
+  })
+
   changeFilter(filterString: FilterType) {
     this.filter.set(filterString);
+  }
+
+  addTodo() {
+    const newTodoTitle = this.newTodo.value.trim();
+    if (this.newTodo.valid && newTodoTitle !== '') {
+      this.todoList.update((prev_todos) => {
+        return[
+          ...prev_todos,
+          {id: Date.now(), title: newTodoTitle, completed: false}
+        ];
+      });
+      this.newTodo.reset();
+    } else {
+      this.newTodo.reset();
+      alert('Por favor, introduce un texto en el campo de entrada.');
+    }
   }
 
 }
